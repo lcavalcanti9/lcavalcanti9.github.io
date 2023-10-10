14a8b07a39f0869fe1ab20f64aa2f28d71f0cbc4
lcavalcanti9 <101212715+lcavalcanti9@users.noreply.github.com>
2023-10-09 22:44:44 -0300

    movement prototyping 1

--- palace.js
+++ palace.js
@@ -1,2 +1,22 @@
-var palace = document.getElementById("palace");
-var palacectx = palace.getContext("2d");
+const palace = document.getElementById("palace");
+const ctx = palace.getContext("2d");
+
+let posx = 50;
+let posy = 20;
+
+function drawCharac(){
+    ctx.beginPath();
+    ctx.rect(posx, posy, 10, 10);
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function getToChair(){
+    ctx.clearRect(0,0,500,500);
+    posy+=0.5;
+    posx+=0.5;
+    drawCharac();
+    requestAnimationFrame(getToChair);
+}
+
+getToChair();
