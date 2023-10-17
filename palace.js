74f8de6c9a56ba0467b3331f0865a735e3a17db0
lara <101212715+lcavalcanti9@users.noreply.github.com>
2023-10-17 18:28:27 -0300

    actual background lets goooooooooooo

--- palace.js
+++ palace.js
@@ -1,3 +1,24 @@
+//fetches canvas from HTML
+//*const = variable that never changes, will stay the same
+const palace = document.getElementById("palace");
+// "rendering interface" variable
+const ctx = palace.getContext("2d");
+
+//these will make the canvas size according to the window/monitor size
+palace.width = window.innerWidth;
+palace.height = window.innerHeight;
+
+//prints height and width of window - just to help programmer, no functionality beyond that
+console.log(palace.width= window.innerWidth); //1366 - my personal window sizes, serve as guidelines for other numbers and positions
+console.log(palace.height=window.innerHeight); //651
+
+//fetch images from folder and put them into variables
+var backgroundi = new Image();
+backgroundi.src="palacebg.PNG";
+var rugi = new Image();
+rugi.src = "rug.PNG";
+var wchairsi = new Image();
+wchairsi.src= "waitingchairs.PNG";
 var gamechairsi = new Image();
 gamechairsi.src = "gamechairs.PNG";
 var curtainsi = new Image();
@@ -42,6 +63,13 @@ function getToChair(a, b, c){
             c=1;
         }
     }
+    drawBG();
+    drawCharac();
+    requestAnimationFrame(getToChair);
+}
+
+getToChair(60, 30, chair1);
+console.log(chair1);
     drawBG();
     drawCharac();
     requestAnimationFrame(getToChair);
