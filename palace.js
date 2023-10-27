612a14d9fbc5a4d3fd559d934c3b3a3ba5c44161
lara <101212715+lcavalcanti9@users.noreply.github.com>
2023-10-26 22:08:34 -0300

    blob works!!!

--- palace.js
+++ palace.js
@@ -12,7 +12,7 @@ palace.height = window.innerHeight;
 console.log(palace.width= window.innerWidth); //1366 - my personal window sizes, serve as guidelines for other numbers and positions
 console.log(palace.height=window.innerHeight); //651
 
-//fetch images from folder and put them into variables
+// BACKGROUND fetch images from folder and put them into variables - innefficient - CHANGE LATER
 var backgroundi = new Image();
 backgroundi.src="palacebg.PNG";
 var rugi = new Image();
@@ -24,17 +24,35 @@ gamechairsi.src = "gamechairs.PNG";
 var curtainsi = new Image();
 curtainsi.src= "curtains.PNG";
 
+// FISH options: bob, fefito, paquito, baiacu
+var bobwalk = new Image();
+bobwalk.src="bobgoeswalk.GIF";
+var blushbob = new Image();
+blushbob.src="blushblob.PNG";
+var facemaskbob = new Image();
+facemaskbob.src="facemaskblob.PNG";
+var browbob = new Image();
+browbob.src="browblob.PNG";
+var normalbob = new Image();
+normalbob.src="normalblob.PNG";
+
 // initial positions of hypothetical cube/character
 let posx = 360;
 let posy = 75;
 
-//chair variables
+// how many fish fish
+var afish = 0;
+
+// chair variables
 var chair1 = 0;
 var chair2 = 0;
 var chair3 = 0;
 var chair4 = 0;
 
-//this function compiles all of the background images at the same time - easier as one command
+// level
+var level = 1;
+
+// this function compiles all of the background images at the same time - easier as one command
 function drawBG(){
     ctx.drawImage(backgroundi,0, 0,window.innerWidth, window.innerHeight);
     ctx.drawImage(rugi,0, 0,window.innerWidth, window.innerHeight);
@@ -43,32 +61,92 @@ function drawBG(){
     ctx.drawImage(gamechairsi,0, 0,window.innerWidth, window.innerHeight);
 }
 
+// draws sprite
 function drawCharac(){
-    ctx.beginPath();
-    ctx.rect(posx, posy, 20, 20);
-    ctx.stroke();
-    ctx.closePath();
+    ctx.drawImage(bobwalk, posx, posy, 75, 75);
 }
 
+// this function only exists because the request animation frame one won't allow getToChair() to have parameters
 function reachChair(){
-    getToChair(300,750,chair1);
+    getToChair(305,770,chair1);
 }
 
+function leavingChair(){
+    leaveChair(50,360,chair1);
+}
+
+function blobM(){
+    ctx.clearRect(0,0,window.innerWidth,window.innerHeight);
+    console.log("blob");
+    drawBG();
+    ctx.drawImage(normalbob, 0,0,window.innerWidth, window.innerHeight);
+    window.addEventListener("keydown", e=>{
+        console.log(e.key);
+        if (e.key=="1"){
+            ctx.clearRect(0,0,window.innerWidth,window.innerHeight);
+            console.log("blob");
+            drawBG();
+            ctx.drawImage(facemaskbob, 0, 0, window.innerWidth, window.innerHeight);
+        }
+        else if(e.key=="2"){
+            ctx.clearRect(0,0,window.innerWidth,window.innerHeight);
+            console.log("blob");
+            drawBG();
+            ctx.drawImage(blushbob, 0, 0, window.innerWidth, window.innerHeight);
+        }
+        else if(e.key=="3"){
+            ctx.clearRect(0,0,window.innerWidth,window.innerHeight);
+            console.log("blob");
+            drawBG();
+            ctx.drawImage(browbob, 0, 0, window.innerWidth, window.innerHeight);
+        }
+        else{
+            palace.width = window.innerWidth;
+            palace.height = window.innerHeight;
+            leaveChair();
+        };
+    })
+}
+
+function makeupButton(){
+    palace.width = window.innerWidth;
+    palace.height = window.innerHeight;
+    ctx.clearRect(0,0,500,500);
+    drawBG();
+    drawCharac();
+    ctx.beginPath();
+    ctx.strokeRect(760, 380, 20, 20);  
+
+    window.addEventListener("keydown", e=>{
+        console.log(e.key);
+        if (e.key=="1"){
+            blobM();
+        }
+    });
+}
+
+// getting to chair 
 function getToChair(a, b, chair){
-    console.log("a=", a);
+    palace.width = window.innerWidth;
+    palace.height = window.innerHeight;
     ctx.clearRect(0,0,500,500);
     if (posy<a){
         console.log("yes");
-        posy+=0.5; 
+        posy+=0.75; 
     }
     else if (posy>=a){
         console.log("no");
         if (posx<=b){
-            posx+=0.5;
+            posx+=0.75;
+            bobwalk.src ="bobgoeswalk90.GIF";
         }
         else if (posx>b){
             chair=1;
             console.log(chair);
+            console.log("yes")
+            bobwalk.src="bobgoeswalk.GIF";  
+            makeupButton(); 
+            exit;
         }
     }
     drawBG();
@@ -76,5 +154,27 @@ function getToChair(a, b, chair){
     requestAnimationFrame(reachChair);
 }
 
-getToChair(300, 750, chair1);
+function leaveChair(a,b,c){
+    palace.width = window.innerWidth;
+    palace.height = window.innerHeight;
+    ctx.clearRect(0,0,500,500);
+    if (posx>=b){
+        bobwalk.src="bobgoeswalk270.GIF";
+        posx-=0.75;
+        c=0;
+    }
+    else if (posx<b){
+        bobwalk.src="bobgoeswalk180.GIF";
+        posy-=0.75;
+    }
+    else if (posy>a){
+        exit
+    }
+    drawBG();
+    drawCharac();
+    requestAnimationFrame(leavingChair);                 
+}
+
+// calling
+getToChair(300, 750, chair1, "bobwalk");
 console.log(chair1);
